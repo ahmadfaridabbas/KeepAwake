e7c314720d93084c7a22604ba617453b56794793
ahmadfaridabbas <ahmadfaridabbas@gmail.com>
2025-08-11 10:00:33 -0400

    Initial Commit

---
+++ KeepAwake/KeepAwakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepAwakeApp.swift
+//  KeepAwake
+//
+//  Created by Ahmad Farid Abbas on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct KeepAwakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeepAwakeTests/KeepAwakeTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeepAwakeTests.swift
+//  KeepAwakeTests
+//
+//  Created by Ahmad Farid Abbas on 8/11/25.
+//
+
+import Testing
+@testable import KeepAwake
+
+struct KeepAwakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
